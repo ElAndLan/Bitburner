f88fae8b3f46f718d01036c278063cca15b330b3
ElAndLan <97379737+ElAndLan@users.noreply.github.com>
2022-01-09 01:08:33 -0800

    Create purchase-server.js
    Used to automatically purchase servers of certain sizes as long as you have enough money.
    
    Bugs:: Doesn't ACTUALLY automatically do it, and instead purchases just one.

---
+++ purchase-server.js
@@ -0,0 +1,25 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	// Variable to hold the size of the server you want to buy
+	let serverSize = 16
+
+	// Iterator for the while loop
+	let i = ns.getPurchasedServers();
+	// Cost of the server size you want to automate buying
+	let purchasedServerCost = ns.getPurchasedServerCost(serverSize);
+
+	while (i.length < ns.getPurchasedServerLimit()) {
+		if (ns.getServerMoneyAvailable("home") > purchasedServerCost) {
+			var hostName = ns.purchaseServer(`${serverSize}gb-server ` + i.length, serverSize)
+			await ns.scp("worms.ns", hostName);
+			await ns.scp("hackservers.ns", hostName);
+			ns.exec("worms.ns -t 6 phantasy silver-helix omega-net iron-gym zer0", hostName);
+			++i;
+		} else {
+			ns.alert(`A server of size ${serverSize}gb costs ${purchasedServerCost}`);
+			sleep(5);
+		}
+	}
+
+	if (i.length === ns.getPurchasedServerLimit()) ns.alert("You are at your limit of servers!");
+}
